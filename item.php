0f7604e38298ece74bd798604e7835f1cb6a8128
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-05-19 15:16:40 +0200

    Ajout fichiers de base

---
+++ item.php
@@ -0,0 +1,11 @@
+<?php
+    $nom = "Ecouteur";
+    $prix = 12;
+    $url = "https://boutique.iservices.fr/7362/ecouteurs-sans-fil-music-pods-3eme-gen.jpg";
+?>
+
+<div>
+    <h1><?php echo $nom ?></h1>
+    <p><?php echo $prix ?></p>
+    <img src=<?php echo $url ?> alt="image ecouteur"/>
+</div>
