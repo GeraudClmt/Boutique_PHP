24dc01765bf2b71c125651f38fe78348d26bf524
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-05-20 14:32:38 +0200

    Changement des prix en centimes

--- datas/multidimentional-catalog.php
+++ datas/multidimentional-catalog.php
@@ -4,21 +4,21 @@ require_once 'header.php';
 $products = [
     "corde" => [
         "name" => "Corde d'escalade",
-        "price" => 120,
+        "price" => 12000,
         "weight" => 3000,
         "discount" => 10,
         "picture_url" => "https://contents.mediadecathlon.com/p2615071/k$b2f1933c71b675afddb50d80df8b4a9b/sq/corde-descalade-95-mm-x-80-m-vertika.jpg?format=auto&f=800x0"
     ],
     "piolet" => [
         "name" => "Piolet",
-        "price" => 150,
+        "price" => 15000,
         "weight" => 500,
         "discount" => 5,
         "picture_url" => "https://skitour.fr/matos/photos/5508.jpg"
     ],
     "crampons" => [
         "name" => "Crampons",
-        "price" => 160,
+        "price" => 16000,
         "weight" => 600,
         "discount" => null,
         "picture_url" => "https://www.montagnes-magazine.com/media/MATOS/2021/novembre/petzl.jpeg"
