ee5fe791988a5c5d2a7a2eeb48fc0e0b236f0752
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-05-28 13:22:40 +0200

    Ajout des fonctions pour récupéré les données dans la bdd

--- boutique.php
+++ boutique.php
@@ -1,10 +1,12 @@
 <?php
 require_once './datas/head.php';
+
 echo head("Boutique", "Des milliers d'articles sont displonible.");
 ?>
 <body>
     <?php require_once 'header.php' ?>
     <main >
+        <?php require_once 'datas/categories.php' ?>
         <form class="listeBoutique" action="cart.php" method="POST">
             <?php //require_once 'datas/catalog-with-keys.php' ?>
             <?php require_once 'datas/multidimentional-catalog.php' ?>
